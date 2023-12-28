4229af59eb482567c397c36b9126879e3ef760a3
carlos731 <57493254+carlos731@users.noreply.github.com>
2023-12-28 10:12:23 -0300

    Aula 260 - Criando tela de onborading

---
+++ app/src/main/java/com/mobiledevchtsca/movieapp/presenter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mobiledevchtsca.movieapp.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.mobiledevchtsca.movieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
