54d1fa8bba517d16c2712bb73776ab8777c4012a
carlos731 <57493254+carlos731@users.noreply.github.com>
2023-12-28 18:46:38 -0300

    Aula 266 - Implementando Dagger Hilt

--- app/src/main/java/com/mobiledevchtsca/movieapp/presenter/auth/login/LoginFragment.kt
+++ app/src/main/java/com/mobiledevchtsca/movieapp/presenter/auth/login/LoginFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mobiledevchtsca.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     override fun onCreateView(
