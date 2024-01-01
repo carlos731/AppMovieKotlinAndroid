43b20bae6884a181bb2185663b84a2d7ed04e5b7
carlos731 <57493254+carlos731@users.noreply.github.com>
2024-01-01 13:13:56 -0300

    Aula 279 - Configurando Adapter Movie

---
+++ app/src/main/java/com/mobiledevchtsca/movieapp/presenter/main/home/adapter/MovieAdapter.kt
@@ -0,0 +1,53 @@
+package com.mobiledevchtsca.movieapp.presenter.main.home.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mobiledevchtsca.movieapp.databinding.MovieItemBinding
+import com.mobiledevchtsca.movieapp.domain.model.Movie
+
+class MovieAdapter(
+    private val context: Context
+): ListAdapter<Movie, MovieAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Movie>(){
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    inner class MyViewHolder(val binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            MovieItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val movie = getItem(position)
+
+        // https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+        // https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+        Glide
+            .with(context)
+            .load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
+            .into(holder.binding.movieImage)
+    }
+
+
+}
