b2cb6681a0d84592e2ecfad2c95af01204970060
carlos731 <57493254+carlos731@users.noreply.github.com>
2024-01-01 19:30:53 -0300

    Aula 283 - Configurando Chamadas da API

---
+++ app/src/main/java/com/mobiledevchtsca/movieapp/data/api/ServiceApi.kt
@@ -0,0 +1,22 @@
+package com.mobiledevchtsca.movieapp.data.api
+
+import com.mobiledevchtsca.movieapp.data.model.BasePaginationRemote
+import com.mobiledevchtsca.movieapp.data.model.GenreResponse
+import com.mobiledevchtsca.movieapp.data.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ServiceApi {
+    @GET("genre/movie/list")
+    suspend fun getGenres(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String?,
+    ): GenreResponse
+
+    @GET("discover/movie")
+    suspend fun getMovieByGenre(
+        @Query("api_key") apiKey: String,
+        @Query("with_genres") genreId: Int?,
+        @Query("language") language: String?,
+    ): BasePaginationRemote<List<MovieResponse>>
+}
