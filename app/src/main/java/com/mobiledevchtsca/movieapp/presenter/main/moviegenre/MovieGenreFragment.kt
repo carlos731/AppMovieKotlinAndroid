f53d6ccd4e8072ceed05f6eecb0c5bb97fcde856
carlos731 <57493254+carlos731@users.noreply.github.com>
2024-01-03 11:42:27 -0300

    Aula 293 - Listando Filmes na Tela de Listagem por Gênero

--- app/src/main/java/com/mobiledevchtsca/movieapp/presenter/main/moviegenre/MovieGenreFragment.kt
+++ app/src/main/java/com/mobiledevchtsca/movieapp/presenter/main/moviegenre/MovieGenreFragment.kt
@@ -1,22 +1,32 @@
 package com.mobiledevchtsca.movieapp.presenter.main.moviegenre
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import com.mobiledevchtsca.movieapp.databinding.FragmentMovieGenreBinding
+import com.mobiledevchtsca.movieapp.presenter.main.bottombar.home.adapter.MovieAdapter
+import com.mobiledevchtsca.movieapp.util.StateView
 import com.mobiledevchtsca.movieapp.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MovieGenreFragment : Fragment() {
 
     private var _binding: FragmentMovieGenreBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MovieGenreViewModel by viewModels()
+
     private val args: MovieGenreFragmentArgs by navArgs()
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +39,45 @@ class MovieGenreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initToolbar(toolbar = binding.toolbar)
+
+        initRecycler()
+
+        getMoviesByGenre()
     }
+
+    private fun initRecycler() {
+        movieAdapter = MovieAdapter(requireContext())
+
+        with(binding.recyclerMovies) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            setHasFixedSize(true)
+            adapter = movieAdapter
+        }
+    }
+
+    private fun getMoviesByGenre() {
+        viewModel.getMoviesByGenre(args.genreId).observe(viewLifecycleOwner) { stateView ->
+            when(stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+                is StateView.Success -> {
+                    binding.progressBar.isVisible = false
+                    movieAdapter.submitList(stateView.data)
+                }
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                }
+            }
+        }
+    }
+
+    /*
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    */
 
 }
